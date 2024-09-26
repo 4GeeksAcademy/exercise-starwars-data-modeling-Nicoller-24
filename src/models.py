a1eabae32718b1364328761dc07937713167f6a9
Nicoller-24 <166450090+Nicoller-24@users.noreply.github.com>
2024-09-26 21:37:51 +0000

    Proyecto finalizado

--- src/models.py
+++ src/models.py
@@ -7,26 +7,58 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Personajes(Base):
+    __tablename__ = 'personajes'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    height = Column(String(250))
+    mass = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    skin_color = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    birth_year = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+
+class PersonajesFavoritos(Base):
+    __tablename__ = 'personajesFavoritos'
+    id = Column(Integer, primary_key=True)
+    user_favorito = Column(Integer, ForeignKey('usuario.id'))
+    usuario = relationship("Usuario")
+    characters_favorito = Column(Integer, ForeignKey('personajes.id'))
+    personajes = relationship("Personajes")
+
+
+class PlanetasFavoritos(Base):
+    __tablename__ = 'planetasFavoritos'
+    id = Column(Integer, primary_key=True)
+    user_favorito = Column(Integer, ForeignKey('usuario.id'))
+    usuario = relationship("Usuario")
+    characters_favorito = Column(Integer, ForeignKey('planetas.id'))
+    planetas = relationship("Planetas")
+
+
+class Planetas(Base):
+    __tablename__ = 'planetas'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    rotation_period = Column(String(250))
+    orbital_period = Column(String(250), nullable=False)
+    diameter = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    gravity = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+
+class Usuario(Base):
+    __tablename__ = 'usuario'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    pasword = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+## Generar el diagrama ER a partir de la base de datos
 render_er(Base, 'diagram.png')
